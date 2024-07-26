3aab9ca6a5eb76f9300a38108987a319e4bf9188
pratchata <ratchata.pyc@gmail.com>
2024-07-26 16:03:59 +0700

    edit deploy

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import "dotenv/config";
+// import "dotenv/config";
+// import express from "express";
+// import cors from "cors";
+// import notFoundHanlder from "./middlewares/notFound";
+// import errorhandler from "./middlewares/error";
+// import todoRoutes from "./routes/todo-route";
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/todo", todoRoutes);
+
+// app.use(notFoundHanlder);
+// app.use(errorhandler);
+
+// const port = process.env.PORT;
+// app.listen(port, () => // console.log(`Server is running on port ${port}`));
+
+// src/index.ts
 import express from "express";
 import cors from "cors";
-import notFoundHanlder from "./middlewares/notFound";
-import errorhandler from "./middlewares/error";
+import notFoundHandler from "./middlewares/notFound";
+import errorHandler from "./middlewares/error";
 import todoRoutes from "./routes/todo-route";
 
 const app = express();
@@ -10,10 +30,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/todo", todoRoutes);
+app.use("/todos", todoRoutes);
 
-app.use(notFoundHanlder);
-app.use(errorhandler);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
